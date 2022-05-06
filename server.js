8aff157da3d31fa44409fb6115cf465811ac259c
thetryworks <natesmyth1@gmail.com>
2022-05-06 13:53:45 -0500

    added first inquirer question

--- server.js
+++ server.js
@@ -12,3 +12,23 @@ const connection = mysql.createConnection({
   },
     console.log('Connected to the employee_db database.')
   );
+
+function start(){
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'start',
+                message: "What would you like to do?",
+                choices: 
+                ['View all departments', 
+                'View all roles', 
+                'View all employees', 
+                'Add a department', 
+                'Add a role', 
+                'Add an employee', 
+                'Update an employee role']
+            }
+        ])
+};
+start();
