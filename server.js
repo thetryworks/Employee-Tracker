c99bb5ae4376dc977406baa6e140406829e21e0e
thetryworks <natesmyth1@gmail.com>
2022-05-06 12:13:46 -0500

    got MySQL connection established

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+//packages using for this project
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const consoleTables = require('console.table');
+
+//basic way to connect the application to the MySQL database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password1234',
+    database: 'employee_db'
+  },
+    console.log('Connected to the employee_db database.')
+  );
