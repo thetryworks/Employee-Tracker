214e84f4ff854a2277ea9d2206cb2728972c7e9e
thetryworks <natesmyth1@gmail.com>
2022-05-07 19:14:27 -0500

    add a role function working

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const connection = mysql.createConnection({
   },
     console.log('Connected to the employee_db database.')
   );
-
+// starts the node application
 function start(){
     inquirer
         .prompt([
@@ -56,7 +56,7 @@ function start(){
         })
 };
 
-
+//Displays a list of the departments
 function viewAllDepartments(){
     connection.query('SELECT * FROM department', function(err, res){
         if(err) throw err;
@@ -64,6 +64,7 @@ function viewAllDepartments(){
         start();
     })
 };
+//Displays a list of all the roles
 function viewAllRoles(){
     connection.query('SELECT title, role_id, department_name, salary FROM role INNER JOIN department ON role.department_id = department.id', function(err, res){
         if(err) throw err;
@@ -71,6 +72,7 @@ function viewAllRoles(){
         start();
     })
 };
+//Displays a list of all the employees with the desired columns displayed
 function viewAllEmployees(){
     connection.query('SELECT employee_id, first_name, last_name, title, department_name, salary FROM employee INNER JOIN role ON employee.role_id = role.role_id INNER JOIN department ON role.department_id = department.id', function (err, res){
         if(err) throw err;
@@ -78,6 +80,7 @@ function viewAllEmployees(){
         start();
     })
 };
+//Uses an INSERT INTO statement to add new departments
 function addADepartment(){
     inquirer
         .prompt([
@@ -98,7 +101,53 @@ function addADepartment(){
             )
         })
 };
+//adds a new role
 function addARole(){
+    inquirer
+        .prompt([
+            {
+                name: 'role',
+                type: 'input',
+                message: 'What role would you like to add?'
+            },
+            {
+                name: 'salary',
+                type: 'number',
+                message: 'Enter Salary',
+                validate: function(value){
+                    if(isNaN(value) === false){
+                        return true;
+                    }
+                    return false;
+                }
+            },
+            {
+                name: 'department_id',
+                type: 'number',
+                message: 'Enter department_id',
+                validate: function(value){
+                    if(isNaN(value) === false){
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        ]).then(function(answer){
+            connection.query(
+                'INSERT INTO role SET ?',
+                {
+                    title: answer.role,
+                    salary: answer.salary,
+                    department_id: answer.department_id
+                },
+                function(err){
+                    if(err) throw err;
+                    console.log('New role added named '+ answer.role);
+                    start();
+                }
+            )
+        })
+
 };
 function addAnEmployee(){
 };
